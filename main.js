c171f96466955425d1a9eae1344bc5455385b5ba
Shristisingh14 <84385618+Shristisingh14@users.noreply.github.com>
2022-11-09 16:25:52 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 leftWristX = 0;
 leftWristY = 0;
-
+scoreLeftWrist = 0;
 rightWristX = 0;
 rightWristY = 0;
 
@@ -23,6 +23,8 @@ function gotPoses(results) {
     if(results.length >0)
     {
         console.log(results);
+        scoreLeftWrist = results[0].pose.keypoints[9].score;
+        console.log("scoreLeftWrist = " + scoreLeftWrist);
 
         rightWristX = results[0].pose.rightWrist.x;
         rightWristY = results[0].pose.rightWrist.y;
@@ -31,7 +33,7 @@ function gotPoses(results) {
         leftWristX = results[0].pose.leftWrist.x;
         leftWristY = results[0].pose.leftWrist.y;
         console.log("leftWristX = " + leftWristX + "leftWristY = " + leftWristY);
-    }
+    } 
 }
 
 song = "";
@@ -42,6 +44,19 @@ function preload() {
 
 function draw() {
     image(video, 0, 0, 600, 500);
+
+    fill("#FF0000");
+    stroke("#FF0000");
+
+    if(scoreLeftWrist > 0.2)
+    {
+        circle(leftWristX, leftWristY, 20);
+        InNumberleftWristY = Number(leftWristY);
+        remove_decimals = floor(InNumberleftWristY);
+        volume = remove_decimals/500;
+        document.getElementById("volume").innerHTML = "Volume = " + volume;
+        song.setVolume(volume);
+    }
 }
 
 function play() {
