ab7aab3b14966b7192093a59c42f6c6fbcd80441
Shristisingh14 <84385618+Shristisingh14@users.noreply.github.com>
2022-11-12 16:35:01 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
 leftWristX = 0;
 leftWristY = 0;
+scoreRightWrist = 0;
 scoreLeftWrist = 0;
 rightWristX = 0;
+ 
 rightWristY = 0;
+
 
 function setup() {
     canvas = createCanvas(600, 500);
@@ -25,6 +28,9 @@ function gotPoses(results) {
         console.log(results);
         scoreLeftWrist = results[0].pose.keypoints[9].score;
         console.log("scoreLeftWrist = " + scoreLeftWrist);
+
+        scoreRightWrist = results[0].pose.keypoints[10].score;
+        console.log("scoreRightWrist = " + scoreRightWrist);
 
         rightWristX = results[0].pose.rightWrist.x;
         rightWristY = results[0].pose.rightWrist.y;
@@ -56,6 +62,42 @@ function draw() {
         volume = remove_decimals/500;
         document.getElementById("volume").innerHTML = "Volume = " + volume;
         song.setVolume(volume);
+    }
+
+    fill("#FF0000");
+    stroke("#FF0000");
+    if(scoreRightWrist > 0.2)
+    {
+        circle(rightWristX, rightWristY, 20);
+
+        if(rightWristY >0 && rightWristY <=100)
+        {
+            document.getElementById("speed").innerHTML = "speed = 0.5x";
+            song.rate(0.5);
+        }
+        else if(rightWristY >100 && rightWristY <=200)
+        {
+            document.getElementById("speed").innerHTML = "speed = 1x";
+            song.rate(1);
+        }
+        else if(rightWristY >200 && rightWristY <=300)
+        {
+            document.getElementById("speed").innerHTML = "speed = 1.5x";
+            song.rate(1.5);
+        }
+        
+        else if(rightWristY >300 && rightWristY <=400)
+        {
+            document.getElementById("speed").innerHTML = "speed = 2x";
+            song.rate(2);
+        }
+        else if(rightWristY >400 && rightWristY <=500)
+        {
+            document.getElementById("speed").innerHTML = "speed = 2.5x";
+            song.rate(2.5);
+        }
+        
+
     }
 }
 
